0bfbc584939c6d79a427c7a3651f669c314413ce
Will Kumagawa <williamkumagawa@gmail.com>
2023-11-29 13:01:12 -0700

    added css and js to page

---
+++ js/link.js
@@ -0,0 +1,52 @@
+document.getElementById('ablink').onmouseover = function(e) {
+    document.getElementById('ablink').style.fontSize = '2.5rem'
+}
+
+document.getElementById('ablink').onmouseout = function(e) {
+    document.getElementById('ablink').style.fontSize = ''
+}
+
+document.getElementById('prolink').onmouseover = function(e) {
+    document.getElementById('prolink').style.fontSize = '2.5rem'
+}
+
+document.getElementById('prolink').onmouseout = function(e) {
+    document.getElementById('prolink').style.fontSize = ''
+}
+
+document.getElementById('sklink').onmouseover = function(e) {
+    document.getElementById('sklink').style.fontSize = '2.5rem'
+}
+
+document.getElementById('sklink').onmouseout = function(e) {
+    document.getElementById('sklink').style.fontSize = ''
+}
+
+document.getElementById('conlink').onmouseover = function(e) {
+    document.getElementById('conlink').style.fontSize = '2.5rem'
+}
+
+document.getElementById('conlink').onmouseout = function(e) {
+    document.getElementById('conlink').style.fontSize = ''
+}
+document.getElementById('linkedin').onmouseover = function(e) {
+    document.getElementById('linkedin').style.fontSize = '2.5rem'
+}
+
+document.getElementById('linkedin').onmouseout = function(e) {
+    document.getElementById('linkedin').style.fontSize = ''
+}
+document.getElementById('github').onmouseover = function(e) {
+    document.getElementById('github').style.fontSize = '2.5rem'
+}
+
+document.getElementById('github').onmouseout = function(e) {
+    document.getElementById('github').style.fontSize = ''
+}
+document.getElementById('mail').onmouseover = function(e) {
+    document.getElementById('mail').style.fontSize = '2.5rem'
+}
+
+document.getElementById('mail').onmouseout = function(e) {
+    document.getElementById('mail').style.fontSize = ''
+}
